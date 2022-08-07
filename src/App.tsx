3e5c64ed8b27679fa54119609daf3d47e4b7daff
mmahmud suberu <mahmudsuberu9@gmail.com>
2022-08-07 01:36:18 +0100

    added icon images and modified app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const iconImages = {
+    linkedin: `${process.env.PUBLIC_URL}linkedin.png`,
+    instagram: `${process.env.PUBLIC_URL}instagram (1).png`,
+    twitter: `${process.env.PUBLIC_URL}twitter (1).png`
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h3>merhmood</h3>
+        <ul>
+          <li>Skills</li>
+          <li>Tools</li>
+          <li>Projects</li>
+          <li>Resume</li>
+        </ul>
       </header>
+      <section>
+        <div className='user-media'>
+          <div className='social-media'>
+            <span>
+              <a href=""><img src={iconImages.instagram} alt="" /></a>
+            </span>
+            <span>
+              <a href=""><img src={iconImages.linkedin} alt="" /></a>
+            </span>
+            <span>
+              <a href=""><img src={iconImages.twitter} alt="" /></a>
+            </span>
+          </div>
+          <div className='user-image'>
+            <img src={`${process.env.PUBLIC_URL}profile-img.jpg`} alt="" />
+          </div>
+        </div>
+        <div className='message'>
+          <h1>
+            Welcome to my portfolio website
+          </h1>
+          <p>-Hi i am mahmud suberu, a MERN stack developer and this is my porfolio website, i hope you have a great time getting to know me</p>
+        </div>
+      </section>
+      <section>
+        <div className='skills'>
+          <ul>
+            <li>React</li>
+            <li>Express js</li>
+            <li>Node js</li>
+            <li></li>
+            <li></li>
+          </ul>
+        </div>
+        <div className='tools'></div>
+      </section>
     </div>
   );
 }
