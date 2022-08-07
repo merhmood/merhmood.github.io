66b29d0d203c016475a3569f3fffad262dcd2afd
mmahmud suberu <mahmudsuberu9@gmail.com>
2022-08-07 19:40:54 +0100

    save point 3

--- src/App.tsx
+++ src/App.tsx
@@ -32,16 +32,16 @@ function App() {
         <div className='user-media'>
           <div className='social-media'>
             <span>
-              <a href=""><img src={iconImages.instagram} alt="" /></a>
+              <a href="https://www.instagram.com/merh_mood" target="-blank"><img src={iconImages.instagram} alt="" /></a>
             </span>
             <span>
-              <a href=""><img src={iconImages.linkedin} alt="" /></a>
+              <a href="https://www.linkedin.com/in/mahmud-suberu-684020235/" target="-blank"><img src={iconImages.linkedin} alt="" /></a>
             </span>
             <span>
-              <a href=""><img src={iconImages.twitter} alt="" /></a>
+              <a href="https://twitter.com/merh_mood" target="-blank"><img src={iconImages.twitter} alt="" /></a>
             </span>
             <span>
-              <a href=""><img src={iconImages.github} alt="" /></a>
+              <a href="https://github.com/merhmood" target="-blank"><img src={iconImages.github} alt="" /></a>
             </span>
           </div>
           <div className='user-image'>
@@ -52,7 +52,7 @@ function App() {
           <h1>
             Welcome to my portfolio website
           </h1>
-          <p>-Hi i am mahmud suberu, a MERN stack developer and this is my porfolio website. I hope you have a great time getting to know me</p>
+          <p>-Hi i am mahmud suberu, a Fullstack developer and this is my porfolio website. I hope you have a great time getting to know me</p>
           <button>commencer <span>&#8595;</span></button>
         </div>
       </section>
@@ -66,7 +66,6 @@ function App() {
               return <li key={id}>{name} <img src={img} alt="" /></li>
             }
             )}
-            <li></li>
           </ul>
         </div>
       </section>
@@ -85,6 +84,24 @@ function App() {
             })}
         </div>
       </section>
+      <section>
+        <h2>About</h2>
+        <p>From educating and inspiring talented and hardworking young people on their track towards this career. Philosophically, I believe there are two roles to being a software engineer. First, the facilitator. Someone who helps everyone doing their best work together by creating a psychologically safe and involving environment to collaborate. Then, the developer. Someone who translates the team best working into a desirable, realistic, and measurable product. . I value honest conversations and progressive thinking, and I aim to create things that empower and inspire.</p>
+        <ul>
+          <li>Communication.</li>
+          <li>Critical thinking</li>
+          <li>Adaptability</li>
+          <li>Team work</li>
+        </ul>
+      </section>
+      <section>
+        <h2>Resume</h2>
+        <a href="https://drive.google.com/file/d/1QzOxGErx9qr6hew-_8cxv1rICV0ItCkq/view?usp=sharing" target="_blank" rel="noreferrer">For formal accessibilty, you can get my resume by clicking on this link</a>
+      </section>
+      <footer>
+        &copy; of merhmood 2022
+      </footer>
+      <button className="arrow-up">&#8593;</button>
     </div>
   );
 }
