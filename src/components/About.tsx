78ea68f3ed3d64183c001a53952ea479cb53df03
mmahmud suberu <mahmudsuberu9@gmail.com>
2022-10-22 12:12:07 +0100

    fixed nigeria span

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -7,9 +7,9 @@ const About = () => {
         <h3>About</h3>
         <ul>
             <li>I am an amazing and talented software engineer</li>
-            <li>Great communication, leadership and mentoring skills</li>
-            <li>Interested in trying and learning new stuffs</li>
-            <li>Love stuyding maths and Physics</li>
+            <li>I have great communication, leadership and mentoring skills</li>
+          <li>I am interested in learning and trying out new stuffs</li>
+            <li>Love stuyding Maths and Physics</li>
             <li>
               Love listening to music, 
               playing basketball and travelling
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -18,23 +18,13 @@ const Home = () => {
         </div>
         <span>Mahmud Suberu</span>
         <p>
-          I'm a software engineer from
-
-          <span style={
-            {
-              color: 'green', 
-              fontSize: '0.9rem', 
-              marginLeft: 5, 
-              marginRight: 43
-            }
-          }>
-            Nigeria
-          </span>
-          , current using the MERN Stack for day to day workflow.
-          I build software that scales without being impacted by
-          changes that cut across concerns. I have great feel for
-          Software design and Architect, clean code, Test Driven Development, 
-          DevOps and Agile Framework &#x1F601;	
+          I'm a software engineer from <span>Nigeria</span>
+          , currently using the MERN Stack for my 
+          day to day development workflow.
+          I have great feel for Software Design, 
+          Software Architecture, clean code, 
+          Test Driven Development, DevOps and 
+          Agile Framework &#x1F601;	
 
         </p>
       </div>
